617ec69e57361eea6ab3541ca94a1a60b4b79d6f
Aya-Az <ayaazari04@gmail.com>
2025-01-23 21:32:37 +0100

    guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,12 +2,21 @@
 
 # Function to ask the user to guess the number of files
 function guess_files {
-    # Get the number of files in the current directory
-    local num_files=$(ls -l | grep -v '^d' | wc -l)
+    # Get the number of regular files in the current directory
+    local num_files=$(find . -maxdepth 1 -type f | wc -l)
+
+    # Welcome message
+    echo "Welcome to the Guessing Game!"
 
     # Ask the user for a guess
     echo "Guess how many files are in the current directory:"
     read guess
+
+    # Check for non-numeric input
+    while ! [[ $guess =~ ^[0-9]+$ ]]; do
+        echo "Please enter a valid number."
+        read guess
+    done
 
     # Loop until the user guesses correctly
     while [[ $guess -ne $num_files ]]
@@ -18,6 +27,12 @@ function guess_files {
             echo "Too high! Try again."
         fi
         read guess
+
+        # Check for non-numeric input again
+        while ! [[ $guess =~ ^[0-9]+$ ]]; do
+            echo "Please enter a valid number."
+            read guess
+        done
     done
 
     # Congratulate the user when they guess correctly
@@ -26,3 +41,4 @@ function guess_files {
 
 # Call the function to start the game
 guess_files
+
